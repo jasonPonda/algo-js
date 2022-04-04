c35b983565124d0ae33debf34acde5871a149529
57383 <jasonvinda@gmail.com>
2022-04-04 15:49:22 +0200

    first exos

---
+++ 1.0.js
@@ -0,0 +1,21 @@
+let age = 12;
+
+let brotherAge = age + 13;
+//addition
+console.log(brotherAge);
+
+brotherAge = age - 2;
+//soustraction
+console.log(brotherAge);
+
+brotherAge = age * 3;
+//Multiplication
+console.log(brotherAge);
+
+brotherAge = age / 2;
+//Division
+console.log(brotherAge);
+
+brotherAge = age % 3;
+//modulo
+console.log(brotherAge);
